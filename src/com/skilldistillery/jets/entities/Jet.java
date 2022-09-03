87365b43bb0e141521465f8c42796b9e4324b7d6
Tobi <tobi2045@outlook.com>
2022-09-03 13:09:19 -0600

    initial jetApp

--- src/com/skilldistillery/jets/entities/Jet.java
+++ src/com/skilldistillery/jets/entities/Jet.java
@@ -1,5 +1,80 @@
 package com.skilldistillery.jets.entities;
 
-public class Jet {
+//it is abstract
+public abstract class Jet {
 
+		//fields
+		private String model;
+		private double speed;
+		private int range;
+		private long price;
+		
+		//no argument constructor
+		public Jet() {
+			
+		}
+		
+		//constructor
+		public Jet(String model, double speed, int range, long price) {
+			super();
+			this.model = model;
+			this.speed = speed;
+			this.range = range;
+			this.price = price;
+		}
+		
+		//getters and setters
+		public String getModel() {
+			return model;
+		}
+		
+		public void setModel(String model) {
+			this.model = model;
+		}
+		public double getSpeed() {
+			return speed;
+		}
+		public void setSpeed(double speed) {
+			this.speed = speed;
+		}
+		public int getRange() {
+			return range;
+		}
+		public void setRange(int range) {
+			this.range = range;
+		}
+		public long getPrice() {
+			return price;
+		}
+		public void setPrice(long price) {
+			this.price = price;
+		}
+		
+		//fly method 
+		public void fly() {
+			//print out the details of jets 
+			//calculate jetFlyTime
+			double jetFlyTime = this.range()/this.speed;
+			System.out.println(this.model+"can fly"+jetFlyTime+"before running out of fuel");
+		}
+		
+		//getSpeed method 
+		public double getSpeedInMPH(){
+			double calMach = 0;
+			//calculate speed in MPH or mach
+			return calMach;
+			
+		}
+
+		//to string 
+		@Override
+		public String toString() {
+			return "Jet [model=" + model + ", speed=" + speed + ", range=" + range + ", price=" + price + "]";
+		}
+		
+		
+		
+
+		
+		
 }
